7d61c878bfa74e6860456ab6e614ccfe4a6de416
Thông <trungthong251295@gmail.com>
2024-09-04 11:40:29 +0700

    test

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
-
+#
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
